b4ec2fa015b14ade276394d10697789847f3a23e
蛭子屋双六 <33665602+sugoroku-y@users.noreply.github.com>
2022-11-21 03:45:12 +0900

    temp

--- polyfill/ts/array-polyfill.ts
+++ polyfill/ts/array-polyfill.ts
@@ -1,283 +1,272 @@
-interface ArrayConstructor {
-  isArray(a: any): a is any[];
-  from(arrayLike: any): any[];
-}
-
-interface Array<T> {
-  includes(target: any): boolean;
-  fill(v: any): any[];
-}
-
-(function(this: any) {
-  Array.isArray =
-    Array.isArray ||
-    function isArray(obj: any) {
-    return Object.prototype.toString.call(obj) === '[object Array]';
-  };
-  (Array as any).of =
-    (Array as any).of ||
+(function() {
+  Array.isArray ??=
+    function isArray(obj: unknown): obj is unknown[] {
+      return Object.prototype.toString.call(obj) === '[object Array]';
+    };
+  Array.of ??=
     function of() {
-    return Array.prototype.slice.call(arguments);
-  };
-  (Array as any).from =
-    (Array as any).from ||
-    function from(
-      arrayLike: any,
-      mapFn: (e: any, i: number) => any,
-      thisArg: any
-    ) {
+      return Array.prototype.slice.call(arguments);
+    };
+  Array.from ??=
+    function from<THIS, T, R>(
+      arrayLike: {length: number, [index: number]: T},
+      mapFn?: (this: THIS, e: T, i: number) => R,
+      thisArg?: THIS
+    ): R[] {
       if (
         typeof mapFn !== 'function' &&
         Object.prototype.toString.call(mapFn) !== '[object Function]'
       ) {
         mapFn = e => e;
       }
-    let array;
-    if (Array.isArray(arrayLike)) {
-        array = arrayLike.map(mapFn, thisArg);
-      } else if (
-        typeof arrayLike === 'object' &&
+      // もともとarrayLikeが配列ならmapを呼ぶだけ
+      if (Array.isArray(arrayLike)) {
+        return arrayLike.map(mapFn, thisArg);
+      }
+      // Enumeratorが使えるならつかって使って配列化
+      if (typeof arrayLike === 'object' &&
         (typeof arrayLike.Item === 'function' ||
           typeof arrayLike.item === 'function')
       ) {
-      array = new Array();
+        const array = [];
         let i = 0;
-      for (const e = new Enumerator(arrayLike); !e.atEnd(); e.moveNext()) {
+        for (const e = new Enumerator(arrayLike); !e.atEnd(); e.moveNext()) {
           array.push(mapFn.call(thisArg, e.item(), i++));
+        }
+        return array;
       }
-    } else {
-      const len = +arrayLike.length || 0;
-      array = new Array(len);
-      for (let i = 0; i < len; ++i) {
-          array[i] = mapFn.call(thisArg, arrayLike[i], i);
+      const length = +arrayLike.length || 0;
+      const array = [];
+      for (let i = 0; i < length; ++i) {
+        array[i] = mapFn.call(thisArg, arrayLike[i], i);
       }
-    }
       return array;
-  };
+    };
 
-  Array.prototype.some =
-    Array.prototype.some ||
-    function some(
-      this: any[],
-      callback: (e: any, i: number, a: any[]) => any,
-      thisObj: any
-    ) {
-    for (let i = 0; i < this.length; ++i) {
+  Array.prototype.some ??=
+    function some<T, THIS>(
+      this: T[],
+      callback: (this: THIS, e: T, i: number, a: T[]) => unknown,
+      thisObj: THIS
+    ): boolean {
+      for (let i = 0; i < this.length; ++i) {
         if (!(i in this)) {
           continue;
         }
         if (callback.call(thisObj, this[i], i, this)) {
           return true;
         }
-    }
-    return false;
-  };
-  Array.prototype.every =
-    Array.prototype.every ||
-    function every(
-      this: any[],
-      callback: (e: any, i: number, a: any[]) => any,
-      thisObj: any
-    ) {
-    return !this.some((e, i, a) => !callback.call(thisObj, e, i, a));
-  };
-
-  Array.prototype.reduce =
-    Array.prototype.reduce ||
-    function reduce(
-      this: any[],
-      callback: (r: any, e: any, i: number, a: any[]) => any,
-      initialValue: any
-    ) {
-    let index = 0;
-    let result = 1 < arguments.length ? initialValue : this[index++];
-    for (; index < this.length; ++index) {
-        if (!(index in this)) {
-          continue;
-        }
-      result = callback.call(null, result, this[index], index, this);
-    }
-    return result;
-  };
-  Array.prototype.reduceRight =
-    Array.prototype.reduceRight ||
-    function reduceRight(
-      this: any[],
-      callback: (r: any, e: any, i: number, a: any[]) => any,
-      initialValue: any
-    ) {
-    let index = this.length;
-    let result = 1 < arguments.length ? initialValue : this[--index];
-    while (index-- > 0) {
-        if (!(index in this)) {
-          continue;
-        }
-      result = callback.call(null, result, this[index], index, this);
-    }
-    return result;
-  };
-  Array.prototype.forEach =
-    Array.prototype.forEach ||
-    function forEach(
-      this: any[],
-      callback: (e: any, i: number, a: any[]) => any,
-      thisObj: any
-    ) {
-      this.reduce(
-        (r, e, i, a) => callback.call(thisObj, e, i, a) && false,
-        false
-      );
+      }
+      return false;
     };
-  Array.prototype.filter =
-    Array.prototype.filter ||
-    function filter(
-      this: any[],
-      callback: (e: any, i: number, a: any[]) => any,
-      thisObj: any
+  Array.prototype.every ??=
+    function every<T, THIS>(
+      this: T[],
+      callback: (this: THIS, e: T, i: number, a: T[]) => unknown,
+      thisObj?: THIS
     ) {
-    return this.reduce((r, e, i, a) => {
-        if (callback.call(thisObj, e, i, a)) {
-          r.push(e);
+      for (let i = 0; i < this.length; ++i) {
+        if (!(i in this)) {
+          continue;
         }
-      return r;
-    }, []);
-  };
-  Array.prototype.map =
-    Array.prototype.map ||
-    function map(
-      this: any[],
-      callback: (e: any, i: number, a: any[]) => any,
-      thisObj: any
-    ) {
-    return this.reduce((r, e, i, a) => {
-      r[i] = callback.call(thisObj, e, i, a);
-      return r;
-    }, []);
-  };
+        if (!callback.call(thisObj, this[i], i, this)) {
+          return false;
+        }
+      }
+      return true;
+    };
 
-  const adjustIndex = (
+  Array.prototype.reduce ??=
+    function reduce<T, R>(
+      this: T[],
+      callback: (r: R, e: T, i: number, a: T[]) => R,
+      initialValue: R
+    ) {
+      let index = 0;
+      let result = 1 < arguments.length ? initialValue : this[index++];
+      for (; index < this.length; ++index) {
+        if (!(index in this)) {
+          continue;
+        }
+        result = callback.call(null, result, this[index], index, this);
+      }
+      return result;
+    };
+  Array.prototype.reduceRight ??=
+    function reduceRight<T, R>(
+      this: T[],
+      callback: (r: R, e: T, i: number, a: T[]) => R,
+      initialValue: R
+    ) {
+      let index = this.length;
+      let result = 1 < arguments.length ? initialValue : this[--index];
+      while (index-- > 0) {
+        if (!(index in this)) {
+          continue;
+        }
+        result = callback.call(null, result, this[index], index, this);
+      }
+      return result;
+    };
+  Array.prototype.forEach ??=
+    function forEach<T, THIS>(
+      this: T[],
+      callback: (this: THIS, e: T, i: number, a: T[]) => unknown,
+      thisObj: THIS
+    ) {
+      for (let index = 0; index < this.length; ++index) {
+        if (!(index in this)) {
+          continue;
+        }
+        callback.call(thisObj, this[index], index, this);
+      }
+    };
+  Array.prototype.filter ??=
+    function filter<T, THIS>(
+      this: T[],
+      callback: (e: T, i: number, a: T[]) => unknown,
+      thisObj: THIS
+    ) {
+      const result = [];
+      for (let index = 0; index < this.length; ++index) {
+        if (!(index in this)) {
+          continue;
+        }
+        const element = this[index];
+        if (callback.call(thisObj, element, index, this)) {
+          result.push(element);
+        };
+      }
+      return result;
+    };
+  Array.prototype.map ??=
+    function map<T, R, THIS>(
+      this: T[],
+      callback: (this: THIS, e: T, i: number, a: T[]) => R,
+      thisObj: any
+    ): R[] {
+      const result: R[] = [];
+      for (let index = 0; index < this.length; ++index) {
+        if (!(index in this)) {
+          continue;
+        }
+        result[index] = callback.call(thisObj, this[index], index, this);
+      }
+      return result;
+    };
+
+  function adjustIndex(
+    this: unknown[],
     args: IArguments,
     index: number,
-    length: number,
     defaultValue?: number
-  ) => {
-    let value = args.length > index ? +args[index] : NaN;
-    if (isNaN(value)) {
+  ) {
+    if (index >= args.length) {
       if (defaultValue === undefined) {
         throw new Error('arguments[' + index + '] required');
       }
-      value = defaultValue;
+      return defaultValue;
     }
+    let value = +args[index];
     if (value < 0) {
-      value += length;
+      value += this.length;
       if (value < 0) {
         value = 0;
+      } else if (value > length) {
+        value = this.length;
       }
     }
     return value;
   };
-  (Array.prototype as any).copyWithin =
-    (Array.prototype as any).copyWithin ||
-    function copyWithin(this: any[]) {
-      const target = adjustIndex(arguments, 1, this.length);
-      const start = adjustIndex(arguments, 1, this.length, 0);
-      const end = adjustIndex(arguments, 2, this.length, this.length);
-      const targetEnd = Math.min(target + (end - start), this.length);
-    if (target < start) {
-      for (let i = target; i < targetEnd; ++i) {
-          if (!(i in this)) {
-            continue;
-          }
-        this[i] = this[start + i - target];
+  Array.prototype.copyWithin ??=
+    function copyWithin<T>(this: T[], target: number, start: number, end?: number): T[] {
+      const target = adjustIndex.call(this, arguments, 0);
+      const start = adjustIndex.call(this, arguments, 1, 0);
+      const end = adjustIndex.call(this, arguments, 2, this.length);
+      let i = target;
+      for (const element of this.slice(start, end)) {
+        this[i] = element;
       }
-    } else {
-        for (let i = targetEnd; --i >= target; ) {
-          if (!(i in this)) {
-            continue;
-          }
-        this[i] = this[start + i - target];
+      return this;
+    };
+  Array.prototype.fill ??=
+    function fill<T>(this: T[], value: T, start?: number, end?: number): T[] {
+      const start = adjustIndex.call(this, arguments, 1, 0);
+      const end = adjustIndex.call(this, arguments, 2, this.length);
+      for (let i = start; i < end; ++i) {
+        this[i] = value;
       }
-    }
-    return this;
-  };
-  (Array.prototype as any).fill =
-    (Array.prototype as any).fill ||
-    function fill(this: any[], value: any) {
-      const start = adjustIndex(arguments, 1, this.length, 0);
-      const end = adjustIndex(arguments, 2, this.length, this.length);
-    for (let i = start; i < end; ++i) {
-      this[i] = value;
-    }
-    return this;
-  };
-  (Array.prototype as any).find =
-    (Array.prototype as any).find ||
-    function find(
-      this: any[],
-      pred: (this: any[], e: any, i: number, a: any[]) => any,
-      thisArg: any
-    ) {
-    let result;
-      this.some(
-        (e, i, a) => pred.call(thisArg, e, i, a) && ((result = e), true)
-      );
-    return result;
-  };
-  (Array.prototype as any).findIndex =
-    (Array.prototype as any).findIndex ||
-    function findIndex(
-      this: any[],
-      pred: (this: any[], e: any, i: number, a: any[]) => any,
-      thisArg: any
-    ) {
-    let result = -1;
-      this.some(
-        (e, i, a) => pred.call(thisArg, e, i, a) && ((result = i), true)
-      );
-    return result;
-  };
-  (Array.prototype as any).includes =
-    (Array.prototype as any).includes ||
-    function includes(this: any[], searchElement: any) {
-    for (const e of this) {
-        if (e === searchElement) {
+      return this;
+    };
+  Array.prototype.find ??=
+    function find<T, THIS>(
+      this: T[],
+      pred: (this: THIS, e: T, i: number, a: T[]) => unknown,
+      thisArg: THIS
+    ): T | undefined {
+      for (let i = 0; i < this.length; ++i) {
+        if (!(i in this)) {
+          continue;
+        }
+        const e = this[i];
+        if (pred.call(thisArg, e, i, this)) {
+          return e;
+        }
+      }
+      return undefined;
+    };
+  Array.prototype.findIndex ??=
+    function findIndex<T, THIS>(
+      this: T[],
+      pred: (this: THIS, e: T, i: number, a: T[]) => unknown,
+      thisArg: THIS
+    ): number {
+      for (let i = 0; i < this.length; ++i) {
+        if (!(i in this)) {
+          continue;
+        }
+        const e = this[i];
+        if (pred.call(thisArg, e, i, this)) {
+          return i;
+        }
+      }
+      return -1;
+    };
+  Array.prototype.includes ??=
+    function includes<T>(this: T[], searchElement: T): boolean {
+      for (let i = 0; i < this.length; ++i) {
+        if (!(i in this)) {
+          continue;
+        }
+        if (this[i] === searchElement) {
           return true;
         }
-    }
-    return false;
-  };
-  Array.prototype.indexOf =
-    Array.prototype.lastIndexOf ||
-    function indexOf(this: any[], searchElement: any) {
-      for (
-        let index = adjustIndex(arguments, 1, this.length, 0);
-        index < this.length;
-        ++index
-      ) {
+      }
+      return false;
+    };
+  Array.prototype.indexOf ??=
+    function indexOf<T>(this: T[], searchElement: T, start?: number): number {
+      const start = adjustIndex.call(this, arguments, 1, 0);
+      for (let index = start; index < this.length; ++index) {
         if (this[index] === searchElement) {
           return index;
         }
-    }
-    return -1;
-  };
-  Array.prototype.lastIndexOf =
-    Array.prototype.lastIndexOf ||
-    function lastIndexOf(this: any[], searchElement) {
-      for (
-        let index = adjustIndex(arguments, 1, this.length, this.length - 1);
-        index >= 0;
-        --index
-      ) {
+      }
+      return -1;
+    };
+  Array.prototype.lastIndexOf ??=
+    function lastIndexOf<T>(this: T[], searchElement: T): number {
+      const start = adjustIndex.call(this, arguments, 1, this.length - 1);
+      for (let index = start; index >= 0; --index) {
         if (this[index] === searchElement) {
           return index;
         }
-    }
-    return -1;
-  };
+      }
+      return -1;
+    };
 
-  (Array.prototype as any).entries =
-    (Array.prototype as any).entries ||
-    function entries(this: any[]) {
+  Array.prototype.entries ??=
+    function entries<T>(this: T[]): [number, T][] {
     return this.map((value, index) => [index, value]);
   };
   (Array.prototype as any).keys =
--- polyfill/ts/string-polyfill.ts
+++ polyfill/ts/string-polyfill.ts
@@ -1,206 +1,115 @@
-interface String {
-  padStart(length: number, padding: string): string;
-  padEnd(length: number, padding: string): string;
-  repeat(count: number): string;
-}
-(function() {
-  String.prototype.repeat =
-    String.prototype.repeat ||
-    function repeat(this: string, count: number) {
+(function () {
+  String.prototype.repeat ??=
+    function repeat(this: string, length: number): string {
       if (count < 0) {
         throw new Error('repeat count must be non-negative');
       }
       if (count === Infinity) {
         throw new Error('repeat count must be less than infinity');
       }
-      if (this.length === 0) {
-        if (count < 1) {
-          return '';
-        }
-        throw new Error('empty string');
-      }
-      let r = '';
-      while (count-- > 0) {
-        r += this;
-      }
-      return r;
+      return Array(length).fill(this).join('');
     };
-  const originalSubstr = String.prototype.substr;
+  // もともとsubstrは存在しているが、負の数を指定したときの扱いがES2015のsubstrと違うので上書き
   String.prototype.substr = function substr(
     this: string,
     start: number,
     length?: number
-  ) {
-    if (start < 0) {
-      start += this.length;
-    }
-    length =
-      length !== undefined
-        ? length < 0
-          ? length + this.length - start
-          : length
-        : this.length - start;
-    return originalSubstr.call(this, start, length);
+  ): string {
+    const end = start !== undefined && length !== undefined && length >= 0 ? start + length : length;
+    // なぜかsliceはES2015のものと同じなのでそちらを使用
+    return this.slice(start, end);
   };
-  String.prototype.padStart =
-    String.prototype.padStart ||
-    function padStart(this: string, length: number, paddings?: string) {
+  String.prototype.padStart ??=
+    function padStart(this: string, length: number, paddings?: string): string {
       const count = length - this.length;
       if (count <= 0) {
         return this;
       }
-      paddings = paddings || ' ';
+      paddings ??= ' ';
       return (
         paddings
-          .repeat((count + paddings.length - 1) / paddings.length)
+          .repeat(length / paddings.length + 1)
           .substr(0, count) + this
       );
     };
-  String.prototype.padEnd =
-    String.prototype.padEnd ||
-    function padEnd(this: string, length: number, paddings?: string) {
+  String.prototype.padEnd ??=
+    function padEnd(this: string, length: number, paddings?: string): string {
       const count = length - this.length;
       if (count <= 0) {
         return this;
       }
-      paddings = paddings || ' ';
+      paddings ??= ' ';
       return (
         this +
         paddings
-          .repeat((count + paddings.length - 1) / paddings.length)
+          .repeat(length / paddings.length + 1)
           .substr(0, count)
       );
     };
-  (String.prototype as any).startsWith =
-    (String.prototype as any).startsWith ||
-    function startsWith(this: string, searchString: string, position?: number) {
-      position = position !== undefined ? position : 0;
-      return (
-        this.length - position >= searchString.length &&
-        this.lastIndexOf(searchString, position) === position
-      );
-    };
-  (String.prototype as any).endsWith =
-    (String.prototype as any).endsWith ||
-    function endsWith(this: string, searchString: string, position?: number) {
-      position =
-        (position !== undefined ? position : this.length) - searchString.length;
-      return position >= 0 && this.indexOf(searchString, position) === position;
-    };
-  String.prototype.trim =
-    String.prototype.trim ||
-    function trim(this: string) {
-      const match = /(?:\S(?:.*\S)?)(?=\s*$)/.exec(this);
-      return (match && match[0]) || '';
-    };
-  (String.prototype as any).includes =
-    (String.prototype as any).includes ||
-    function includes(this: string, searchString: string, position?: number) {
-      if (typeof position !== 'number') {
-        position = 0;
+  String.prototype.startsWith ??=
+    function startsWith(this: string, searchString: string, position?: number): boolean {
+      if (!position) {
+        return this.lastIndexOf(searchString, 0) === 0;
       }
-      return (
-        position + searchString.length <= this.length &&
-        this.indexOf(searchString, position) >= 0
-      );
+      return this.slice(position, searchString.length) === searchString;
+        
     };
+  String.prototype.endsWith ??=
+    function endsWith(this: string, searchString: string, position?: number): boolean {
+      if (!position) {
+        const pos = this.length - searchString.length;
+        return this.indexOf(searchString, pos) === pos;
+      }
+      const pos = position - searchString.length;
+      return pos >= 0 && this.slice(pos, position) === searchString;
+    };
+  String.prototype.trim ??=
+    function trim(this: string): string {
+      return /(?:\S(?:.*\S)?)(?=\s*$)/s.exec(this)?.[0] ?? '';
+    };
+  String.prototype.includes ??=
+    function includes(this: string, searchString: string, position?: number):boolean {
+      return this.indexOf(searchString, position) >= 0
+    };
+  const original_split = String.prototype.split
+  // separatorにキャプチャを含む正規正規表現を指定したときの挙動、およびSymbol.splitの扱いがES2015のものと合わないので上書き
   String.prototype.split = function split(
     this: string,
     separator:
       | string
       | RegExp
-      | {[Symbol.split](sss: string, limit?: number): string[]},
+      | { [Symbol.split](sss: string, limit?: number): string[] },
     limit?: number
   ) {
     const str = '' + this;
+    if (typeof separator === 'string') {
+      // 文字列でのsplitは元のメソッドを呼び出し
+      return original_split.call(str, separator, limit);
+    }
     // Symbol.splitがあればそちらを使う
-    if (
-      separator &&
-      typeof separator !== 'string' &&
-      !(separator instanceof RegExp) &&
-      typeof separator[Symbol.split] === 'function'
-    ) {
+    // ※JScriptのRegExpにはSymbol.splitgaがない
+    if (!(separator instanceof RegExp)) {
       return separator[Symbol.split](str, limit);
     }
+    // gをつけて作り直す
+    const pattern = new RegExp(separator.source, separator.flags + 'g');
     // separatorで分割
     let index = 0;
     const result = [];
-    if (separator instanceof RegExp) {
-      // separatorが正規表現の場合
-      // 未分割な文字列の先頭のインデックス
-      let prev = 0;
-      // globalでなかったらgをつけて再作成
-      const re = separator.global
-        ? separator
-        : new RegExp(
-            separator.source,
-            `g${separator.multiline ? 'm' : ''}${
-              separator.ignoreCase ? 'i' : ''
-            }`
-          );
-      // 正規表現のパターンが見つからなくなるまで検索
-      while (true) {
-        // まだ見ていないところから検索
-        re.lastIndex = index;
-        const match = re.exec(str);
-        // 見つからなかったら終了
-        if (!match) {
-          break;
-        }
-        if (match[0].length) {
-          // 見つかったパターンが空文字列でなければ見つかった文字列で分割
-          const next = match.index;
-          const splitted = str.substr(prev, next - prev);
-          result.push(splitted);
-          prev = index = next + match[0].length;
-        } else {
-          // 見つかった文字列が空文字列なら
-          if (index === prev) {
-            // 前回と同じ検索位置だったら次の文字から検索する
-            index = prev + 1;
-            if (index >= str.length) {
-              // もう検索文字が無いときは今の結果をそのまま返す
-              return result;
-            }
-            continue;
-          }
-          // もう検索文字列が無いときは終了
-          if (index >= str.length) {
-            break;
-          }
-          // 分割した文字列を結果に追加
-          const next = index;
-          const splitted = str.substr(prev, next - prev);
-          prev = next;
-          result.push(splitted);
-          ++index;
-        }
-        // サブマッチを結果に追加
-        for (let i = 1; i < match.length; ++i) {
-          result.push(match[i]);
-        }
-      }
-      // 最後の部分を追加
-      result.push(str.substr(prev));
-      return result;
+    for (const match of this.matchAll(pattern)) {
+      if (index === match.index && match[0].length === 0) continue;
+      result.push(str.slice(index, match.index), ...match.slice(1));
+      index = match.index;
     }
-    // セパレータが空文字の場合は1文字ごと分割
-    if (!separator) {
-      return Array(str.length)
-        .fill(0)
-        .map((_, i) => str.substr(i, 1));
-    }
-    // 文字列で分割
-    separator = '' + separator;
-    while (index < str.length) {
-      const next = str.indexOf(separator, index);
-      if (next < 0) {
-        break;
-      }
-      result.push(str.substr(index, next - index));
-      index = next + separator.length;
-    }
-    result.push(str.substr(index));
+    // 最後の部分を追加
+    result.push(str.slice(index));
     return result;
+  };
+  String.prototype.matchAll ??= function* matchAll(_pattern: RegExp): Generator<RegExpExecArray, void> {
+    const pattern = new RegExp(_pattern.source, _pattern.flags);
+    let match;
+    while (!!(match = pattern.exec(this))) {
+      yield match;
+    }
   };
 })();
