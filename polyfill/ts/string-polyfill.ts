bf1f361004e4b03c12129ce476d70caf5d258cca
YEBISUYA Sugoroku <yebisuya@gmail.com>
2022-12-06 02:52:44 +0900

    refactor: 最新のtscでコンパイルできるように修正

--- polyfill/ts/string-polyfill.ts
+++ polyfill/ts/string-polyfill.ts
@@ -1,12 +1,6 @@
-interface String {
-  padStart(length: number, padding: string): string;
-  padEnd(length: number, padding: string): string;
-  repeat(count: number): string;
-}
-(function() {
-  String.prototype.repeat =
-    String.prototype.repeat ||
-    function repeat(this: string, count: number) {
+(function () {
+  String.prototype.repeat ??=
+    function repeat(this: string, count: number): string {
       if (count < 0) {
         throw new Error('repeat count must be non-negative');
       }
@@ -30,7 +24,7 @@ interface String {
     this: string,
     start: number,
     length?: number
-  ) {
+  ): string {
     if (start < 0) {
       start += this.length;
     }
@@ -42,60 +36,58 @@ interface String {
         : this.length - start;
     return originalSubstr.call(this, start, length);
   };
-  String.prototype.padStart =
-    String.prototype.padStart ||
-    function padStart(this: string, length: number, paddings?: string) {
+  String.prototype.padStart ??=
+    function padStart(this: string, length: number, paddings?: string): string {
       const count = length - this.length;
       if (count <= 0) {
         return this;
       }
       paddings = paddings || ' ';
-      return (
-        paddings
-          .repeat((count + paddings.length - 1) / paddings.length)
-          .substr(0, count) + this
-      );
+      const repeat = length / paddings.length + 1;
+      return paddings.repeat(repeat).slice(0, count) + this;
     };
-  String.prototype.padEnd =
-    String.prototype.padEnd ||
-    function padEnd(this: string, length: number, paddings?: string) {
+  String.prototype.padEnd ??=
+    function padEnd(this: string, length: number, paddings?: string): string {
       const count = length - this.length;
       if (count <= 0) {
         return this;
       }
       paddings = paddings || ' ';
+      const repeat = length / paddings.length + 1;
+      return this + paddings.repeat(repeat).slice(0, count);
+    };
+  String.prototype.startsWith ??=
+    function startsWith(
+      this: string,
+      searchString: string,
+      position?: number
+    ): boolean {
+      const pos = position ?? 0;
       return (
-        this +
-        paddings
-          .repeat((count + paddings.length - 1) / paddings.length)
-          .substr(0, count)
+        this.length - pos >= searchString.length &&
+        this.lastIndexOf(searchString, pos) === pos
       );
     };
-  (String.prototype as any).startsWith =
-    (String.prototype as any).startsWith ||
-    function startsWith(this: string, searchString: string, position?: number) {
-      position = position !== undefined ? position : 0;
-      return (
-        this.length - position >= searchString.length &&
-        this.lastIndexOf(searchString, position) === position
-      );
+  String.prototype.endsWith ??=
+    function endsWith(
+      this: string,
+      searchString: string,
+      position?: number
+    ): boolean {
+      const pos = (position ?? this.length) - searchString.length;
+      return pos >= 0 && this.indexOf(searchString, pos) === pos;
     };
-  (String.prototype as any).endsWith =
-    (String.prototype as any).endsWith ||
-    function endsWith(this: string, searchString: string, position?: number) {
-      position =
-        (position !== undefined ? position : this.length) - searchString.length;
-      return position >= 0 && this.indexOf(searchString, position) === position;
-    };
-  String.prototype.trim =
-    String.prototype.trim ||
-    function trim(this: string) {
+  String.prototype.trim ??=
+    function trim(this: string): string {
       const match = /(?:\S(?:.*\S)?)(?=\s*$)/.exec(this);
       return (match && match[0]) || '';
     };
-  (String.prototype as any).includes =
-    (String.prototype as any).includes ||
-    function includes(this: string, searchString: string, position?: number) {
+  String.prototype.includes ??=
+    function includes(
+      this: string,
+      searchString: string,
+      position?: number
+    ): boolean {
       if (typeof position !== 'number') {
         position = 0;
       }
@@ -104,6 +96,7 @@ interface String {
         this.indexOf(searchString, position) >= 0
       );
     };
+  const original_split = String.prototype.split;
   String.prototype.split = function split(
     this: string,
     separator:
@@ -111,8 +104,12 @@ interface String {
       | RegExp
       | {[Symbol.split](sss: string, limit?: number): string[]},
     limit?: number
-  ) {
+  ): string[] {
     const str = '' + this;
+    if (typeof separator === "string") {
+      // 文字列での分割は元のsplitを使う
+      return original_split(separator, limit)
+    }
     // Symbol.splitがあればそちらを使う
     if (
       separator &&
@@ -126,6 +123,16 @@ interface String {
     let index = 0;
     const result = [];
     if (separator instanceof RegExp) {
+      const pattern = new RegExp(separator.source, separator.flags + 'g');
+      let match;
+      while (!!(match = pattern.exec(str))) {
+        if (match[0].length === 0 && match.index === 0) {
+          // 空文字にマッチして
+          continue;
+        }
+        result.push(str.slice(index, match.index), ...match.slice(1));
+        index = match.index + match[0].length
+      }
       // separatorが正規表現の場合
       // 未分割な文字列の先頭のインデックス
       let prev = 0;
@@ -150,7 +157,7 @@ interface String {
         if (match[0].length) {
           // 見つかったパターンが空文字列でなければ見つかった文字列で分割
           const next = match.index;
-          const splitted = str.substr(prev, next - prev);
+          const splitted = str.slice(prev, next);
           result.push(splitted);
           prev = index = next + match[0].length;
         } else {
@@ -170,7 +177,7 @@ interface String {
           }
           // 分割した文字列を結果に追加
           const next = index;
-          const splitted = str.substr(prev, next - prev);
+          const splitted = str.slice(prev, next);
           prev = next;
           result.push(splitted);
           ++index;
@@ -181,14 +188,12 @@ interface String {
         }
       }
       // 最後の部分を追加
-      result.push(str.substr(prev));
+      result.push(str.slice(prev));
       return result;
     }
     // セパレータが空文字の場合は1文字ごと分割
     if (!separator) {
-      return Array(str.length)
-        .fill(0)
-        .map((_, i) => str.substr(i, 1));
+      return Array.from(str, (_, i) => str.charAt(i));
     }
     // 文字列で分割
     separator = '' + separator;
@@ -197,10 +202,26 @@ interface String {
       if (next < 0) {
         break;
       }
-      result.push(str.substr(index, next - index));
+      result.push(str.slice(index, next));
       index = next + separator.length;
     }
-    result.push(str.substr(index));
+    result.push(str.slice(index));
     return result;
   };
+  String.prototype.matchAll =
+    String.prototype.matchAll ||
+    function* matchAll(
+      this: string,
+      pattern: RegExp
+    ): Generator<RegExpMatchArray, void> {
+      if (!pattern.global) {
+        throw new Error(
+          'String.prototype.matchAll called with a non-global RegExp argument'
+        );
+      }
+      let m;
+      while (!!(m = this.match(pattern))) {
+        yield m;
+      }
+    };
 })();
