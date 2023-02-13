2e22d328a5c57270e89b4a875ac3c93031305aea
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-02-14 01:19:06 +0900

    fix: テストケースの名前を実行する式そのものに変更

--- polyfill/test/string.test.ts
+++ polyfill/test/string.test.ts
@@ -1,135 +1,145 @@
 describe('String.prototype.repeat', () => {
-  test(`String.prototype.repeat: abc x 3`, () => {
+  test(`'abc'.repeat(3)`, () => {
     expect('abc'.repeat(3)).toBe('abcabcabc');
   });
-  test(`String.prototype.repeat: abc x 0`, () => {
+  test(`'abc'.repeat(0)`, () => {
     expect('abc'.repeat(0)).toBe('');
   });
-  test(`String.prototype.repeat: abc x NaN`, () => {
+  test(`'abc'.repeat(NaN)`, () => {
     expect('abc'.repeat(NaN)).toBe('');
   });
-  test(`String.prototype.repeat: abc x -1`, () => {
+  test(`() => 'abc'.repeat(-1)`, () => {
     expect(() => 'abc'.repeat(-1)).toThrow();
   });
-  test(`String.prototype.repeat: abc x Infinity`, () => {
+  test(`() => 'abc'.repeat(Infinity)`, () => {
     expect(() => 'abc'.repeat(Infinity)).toThrow();
   });
-  test(`String.prototype.repeat: abc x -Infinity`, () => {
+  test(`() => 'abc'.repeat(-Infinity)`, () => {
     expect(() => 'abc'.repeat(-Infinity)).toThrow();
   });
 });
 
-test(`String.prototype.substr(4)`, () => {
-  expect('abcdefg'.substr(4)).toBe(`efg`);
+describe('String.prototype.substr', () => {
+  test(`'abcdefg'.substr(4)`, () => {
+    expect('abcdefg'.substr(4)).toBe(`efg`);
+  });
+  test(`'abcdefg'.substr(1, 3)`, () => {
+    expect('abcdefg'.substr(1, 3)).toBe(`bcd`);
+  });
+  test(`'abcdefg'.substr(-4, 2)`, () => {
+    expect('abcdefg'.substr(-4, 2)).toBe(`de`);
+  });
+  test(`'abcdefg'.substr(-2, 4)`, () => {
+    expect('abcdefg'.substr(-2, 4)).toBe(`fg`);
+  });
+  test(`'abcdefg'.substr(1, 0)`, () => {
+    expect('abcdefg'.substr(1, 0)).toBe(``);
+  });
+  test(`'abcdefg'.substr(1, -1)`, () => {
+    expect('abcdefg'.substr(1, -1)).toBe(``);
+  });
+  test(`'abcdefg'.substr(1, NaN)`, () => {
+    expect('abcdefg'.substr(1, NaN)).toBe(``);
+  });
+  test(`'abcdefg'.substr(4, undefined)`, () => {
+    expect('abcdefg'.substr(4, undefined)).toBe(`efg`);
+  });
 });
-test(`String.prototype.substr(1, 3)`, () => {
-  expect('abcdefg'.substr(1, 3)).toBe(`bcd`);
+describe('String.prototype.padStart', () => {
+  test(``, () => {
+    expect('abc'.padStart(10)).toBe(`       abc`);
+  });
+  test(``, () => {
+    expect('abcdefghij'.padStart(10)).toBe(`abcdefghij`);
+  });
+  test(``, () => {
+    expect('abcdefghijk'.padStart(10)).toBe(`abcdefghijk`);
+  });
+  test(``, () => {
+    expect('abc'.padStart(10, 'ABC')).toBe(`ABCABCAabc`);
+  });
+  test(``, () => {
+    expect('abcdefghij'.padStart(10, 'ABC')).toBe(`abcdefghij`);
+  });
+  test(``, () => {
+    expect('abcdefghijk'.padStart(10, 'ABC')).toBe(`abcdefghijk`);
+  });
 });
-test(`String.prototype.substr(-4, 2)`, () => {
-  expect('abcdefg'.substr(-4, 2)).toBe(`de`);
+describe('String.prototype.padEnd', () => {
+  test(`'abc'.padEnd(10)`, () => {
+    expect('abc'.padEnd(10)).toBe(`abc       `);
+  });
+  test(`'abcdefghij'.padEnd(10)`, () => {
+    expect('abcdefghij'.padEnd(10)).toBe(`abcdefghij`);
+  });
+  test(`'abcdefghijk'.padEnd(10)`, () => {
+    expect('abcdefghijk'.padEnd(10)).toBe(`abcdefghijk`);
+  });
+  test(`'abc'.padEnd(10, 'ABC')`, () => {
+    expect('abc'.padEnd(10, 'ABC')).toBe(`abcABCABCA`);
+  });
+  test(`'abcdefghij'.padEnd(10, 'ABC')`, () => {
+    expect('abcdefghij'.padEnd(10, 'ABC')).toBe(`abcdefghij`);
+  });
+  test(`'abcdefghijk'.padEnd(10, 'ABC')`, () => {
+    expect('abcdefghijk'.padEnd(10, 'ABC')).toBe(`abcdefghijk`);
+  });
 });
-test(`String.prototype.substr(-2, 4)`, () => {
-  expect('abcdefg'.substr(-2, 4)).toBe(`fg`);
+describe('String.prototype.startsWith', () => {
+  test(`'abcdefghij'.startsWith('abc')`, () => {
+    expect('abcdefghij'.startsWith('abc')).toBe(true);
+  });
+  test(`'abcdefghij'.startsWith('ABC')`, () => {
+    expect('abcdefghij'.startsWith('ABC')).toBe(false);
+  });
+  test(`'abcdefghij'.startsWith('bcd', 1)`, () => {
+    expect('abcdefghij'.startsWith('bcd', 1)).toBe(true);
+  });
+  test(`'abcdefghij'.startsWith('BCD', 1)`, () => {
+    expect('abcdefghij'.startsWith('BCD', 1)).toBe(false);
+  });
+  test(`'abcdefghij'.startsWith('cde', 2)`, () => {
+    expect('abcdefghij'.startsWith('cde', 2)).toBe(true);
+  });
+  test(`'abcdefghij'.startsWith('CDE', 2)`, () => {
+    expect('abcdefghij'.startsWith('CDE', 2)).toBe(false);
+  });
+  test(`'abcdefghij'.startsWith('hij', 7)`, () => {
+    expect('abcdefghij'.startsWith('hij', 7)).toBe(true);
+  });
+  test(`'abcdefghij'.startsWith('HIJ', 7)`, () => {
+    expect('abcdefghij'.startsWith('HIJ', 7)).toBe(false);
+  });
+  test(`'abcdefghij'.startsWith('ijk', 8)`, () => {
+    expect('abcdefghij'.startsWith('ijk', 8)).toBe(false);
+  });
 });
-test(`String.prototype.substr(1, 0)`, () => {
-  expect('abcdefg'.substr(1, 0)).toBe(``);
-});
-test(`String.prototype.substr(1, -1)`, () => {
-  expect('abcdefg'.substr(1, -1)).toBe(``);
-});
-test(`String.prototype.substr(1, NaN)`, () => {
-  expect('abcdefg'.substr(1, NaN)).toBe(``);
-});
-test(`String.prototype.substr(4, undefined)`, () => {
-  expect('abcdefg'.substr(4, undefined)).toBe(`efg`);
-});
-test(`String.prototype.padStart('abc', 10))`, () => {
-  expect('abc'.padStart(10)).toBe(`       abc`);
-});
-test(`String.prototype.padStart('abcdefghij', 10)`, () => {
-  expect('abcdefghij'.padStart(10)).toBe(`abcdefghij`);
-});
-test(`String.prototype.padStart('abcdefghijk', 10)`, () => {
-  expect('abcdefghijk'.padStart(10)).toBe(`abcdefghijk`);
-});
-test(`String.prototype('abc', 10, 'ABC'))`, () => {
-  expect('abc'.padStart(10, 'ABC')).toBe(`ABCABCAabc`);
-});
-test(`String.prototype('abcdefghij', 10, 'ABC'))`, () => {
-  expect('abcdefghij'.padStart(10, 'ABC')).toBe(`abcdefghij`);
-});
-test(`String.prototype('abcdefghijk', 10, 'ABC'))`, () => {
-  expect('abcdefghijk'.padStart(10, 'ABC')).toBe(`abcdefghijk`);
-});
-test(`String.prototype('abc', 10))`, () => {
-  expect('abc'.padEnd(10)).toBe(`abc       `);
-});
-test(`String.prototype('abcdefghij', 10))`, () => {
-  expect('abcdefghij'.padEnd(10)).toBe(`abcdefghij`);
-});
-test(`String.prototype('abcdefghijk', 10))`, () => {
-  expect('abcdefghijk'.padEnd(10)).toBe(`abcdefghijk`);
-});
-test(`String.prototype('abc', 10, 'ABC'))`, () => {
-  expect('abc'.padEnd(10, 'ABC')).toBe(`abcABCABCA`);
-});
-test(`String.prototype('abcdefghij', 10, 'ABC'))`, () => {
-  expect('abcdefghij'.padEnd(10, 'ABC')).toBe(`abcdefghij`);
-});
-test(`String.prototype('abcdefghijk', 10, 'ABC'))`, () => {
-  expect('abcdefghijk'.padEnd(10, 'ABC')).toBe(`abcdefghijk`);
-});
-test(`String.prototype.startsWith('abc')`, () => {
-  expect('abcdefghij'.startsWith('abc')).toBe(true);
-});
-test(`String.prototype.startsWith('ABC')`, () => {
-  expect('abcdefghij'.startsWith('ABC')).toBe(false);
-});
-test(`String.prototype.startsWith('bcd', 1)`, () => {
-  expect('abcdefghij'.startsWith('bcd', 1)).toBe(true);
-});
-test(`String.prototype.startsWith('BCD', 1)`, () => {
-  expect('abcdefghij'.startsWith('BCD', 1)).toBe(false);
-});
-test(`String.prototype.startsWith('cde', 2)`, () => {
-  expect('abcdefghij'.startsWith('cde', 2)).toBe(true);
-});
-test(`String.prototype.startsWith('CDE', 2)`, () => {
-  expect('abcdefghij'.startsWith('CDE', 2)).toBe(false);
-});
-test(`String.prototype.startsWith('hij', 7)`, () => {
-  expect('abcdefghij'.startsWith('hij', 7)).toBe(true);
-});
-test(`String.prototype.startsWith('HIJ', 7)`, () => {
-  expect('abcdefghij'.startsWith('HIJ', 7)).toBe(false);
-});
-test(`String.prototype.startsWith('ijk', 8)`, () => {
-  expect('abcdefghij'.startsWith('ijk', 8)).toBe(false);
-});
-test(`String.prototype.endsWith('hij')`, () => {
-  expect('abcdefghij'.endsWith('hij')).toBe(true);
-});
-test(`String.prototype.endsWith('HIJ')`, () => {
-  expect('abcdefghij'.endsWith('HIJ')).toBe(false);
-});
-test(`String.prototype.endsWith('ghi', 9)`, () => {
-  expect('abcdefghij'.endsWith('ghi', 9)).toBe(true);
-});
-test(`String.prototype.endsWith('GHI', 9)`, () => {
-  expect('abcdefghij'.endsWith('GHI', 9)).toBe(false);
-});
-test(`String.prototype.endsWith('fgh', 8)`, () => {
-  expect('abcdefghij'.endsWith('fgh', 8)).toBe(true);
-});
-test(`String.prototype.endsWith('FGH', 8)`, () => {
-  expect('abcdefghij'.endsWith('FGH', 8)).toBe(false);
-});
-test(`String.prototype.endsWith('abc', 3)`, () => {
-  expect('abcdefghij'.endsWith('abc', 3)).toBe(true);
-});
-test(`String.prototype.endsWith('ABC', 3)`, () => {
-  expect('abcdefghij'.endsWith('ABC', 3)).toBe(false);
-});
-test(`String.prototype.endsWith('@ab', 2)`, () => {
-  expect('abcdefghij'.endsWith('@ab', 2)).toBe(false);
+describe('String.prototype.endsWith', () => {
+  test(`'abcdefghij'.endsWith('hij')`, () => {
+    expect('abcdefghij'.endsWith('hij')).toBe(true);
+  });
+  test(`'abcdefghij'.endsWith('HIJ')`, () => {
+    expect('abcdefghij'.endsWith('HIJ')).toBe(false);
+  });
+  test(`'abcdefghij'.endsWith('ghi', 9)`, () => {
+    expect('abcdefghij'.endsWith('ghi', 9)).toBe(true);
+  });
+  test(`'abcdefghij'.endsWith('GHI', 9)`, () => {
+    expect('abcdefghij'.endsWith('GHI', 9)).toBe(false);
+  });
+  test(`'abcdefghij'.endsWith('fgh', 8)`, () => {
+    expect('abcdefghij'.endsWith('fgh', 8)).toBe(true);
+  });
+  test(`'abcdefghij'.endsWith('FGH', 8)`, () => {
+    expect('abcdefghij'.endsWith('FGH', 8)).toBe(false);
+  });
+  test(`'abcdefghij'.endsWith('abc', 3)`, () => {
+    expect('abcdefghij'.endsWith('abc', 3)).toBe(true);
+  });
+  test(`'abcdefghij'.endsWith('ABC', 3)`, () => {
+    expect('abcdefghij'.endsWith('ABC', 3)).toBe(false);
+  });
+  test(`'abcdefghij'.endsWith('@ab', 2)`, () => {
+    expect('abcdefghij'.endsWith('@ab', 2)).toBe(false);
+  });
 });
