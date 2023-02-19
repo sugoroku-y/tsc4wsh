4db8c37ffed42cc88e0db511db5666db9303aaa8
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-02-19 22:59:15 +0900

    refactor: Numberのpolyfillを書き換え

--- polyfill/ts/number-polyfill.ts
+++ polyfill/ts/number-polyfill.ts
@@ -1,25 +1,21 @@
-interface NumberConstructor {
-  isFinite(o: unknown): o is number;
-  isInteger(o: unknown): o is number;
-  isSafeInteger(o: unknown): o is number;
-}
-
 (function (this: any) {
-  this.Number.EPSILON = Number.EPSILON || 2.2204460492503130808472633361816e-16;
-  this.Number.MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || 9007199254740991;
-  this.Number.MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER || -9007199254740991;
-  Number.isFinite ??= (o =>
-    typeof o === 'number' &&
-    o !== Infinity &&
-    o !== -Infinity &&
-    !isNaN(o)) as typeof Number.isFinite;
-  Number.isInteger ??= (o =>
-    typeof o === 'number' && Math.ceil(o) === o) as typeof Number.isInteger;
+  this.Number.EPSILON ??= Math.pow(2, -52);
+  this.Number.MAX_SAFE_INTEGER ??= Math.pow(2, 53) - 1;
+  this.Number.MIN_SAFE_INTEGER ??= -Number.MAX_SAFE_INTEGER;
+  Number.isFinite ??= isFinite;
+  Number.isInteger ??= isInteger;
   Number.isNaN ??= o => typeof o === 'number' && isNaN(o);
-  Number.isSafeInteger ??= (o =>
-    Number.isInteger(o) &&
-    o <= Number.MAX_SAFE_INTEGER &&
-    o >= Number.MIN_SAFE_INTEGER) as typeof Number.isSafeInteger;
+  Number.isSafeInteger ??= isSafeInteger;
   Number.parseFloat ??= parseFloat;
   Number.parseInt ??= parseInt;
+
+  function isFinite(o: unknown): o is number {
+    return typeof o === 'number' && o !== Infinity && o !== -Infinity && !isNaN(o);
+  }
+  function isInteger(o: unknown): o is number {
+    return typeof o === 'number' && Math.ceil(o) === o;
+  }
+  function isSafeInteger(o: unknown): o is number {
+    return isInteger(o) && o <= Number.MAX_SAFE_INTEGER && o >= Number.MIN_SAFE_INTEGER;
+  }
 })();
